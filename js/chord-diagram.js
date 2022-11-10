c70c2f4526b72b71d2bd7729b90b8d445291aed3
DAVID PATTERSON <u1207184@umail.utah.edu>
2022-11-10 14:45:43 -0700

    Text in a circle based on the teams, working on getting logos instead.

--- js/chord-diagram.js
+++ js/chord-diagram.js
@@ -1,21 +1,41 @@
 class ChordDiagram {
     constructor(loadedData){
         this.loadedData = loadedData;
-        this.teams = new Set();
-        this.margin = {top: 50, right: 50, bottom: 50, left: 50};
+        this.teams = [];
+        this.teamNames = new Set();
+        loadedData.forEach(game => this.teamNames.add(game.Team));
+        this.teamNames.forEach(team => this.teams.push({team: team}) );
+        console.log(this.teams);
+        console.log(this.teamNames);
 
-        for(let game of this.loadedData){
-            this.teams.add(game["Team"]);
+
+        this.margin = {top: 50, right: 50, bottom: 50, left: 50};
+        this.width = 800 - this.margin.left - this.margin.right;
+        this.height = 800 - this.margin.top - this.margin.bottom;
+
+        this.diameter = this.width;
+        this.radius = this.diameter / 2;
+        this.innerRadius = this.radius - 120;
+
+        const scale = d3.scaleLinear()
+            .domain([0, this.teams.length])
+            .range([0, 2 * Math.PI]);
+        
+        for(let [i, d] of this.teams.entries()){
+            var theta = scale(i);
+            d.x = this.radius * Math.sin(theta)+ 350;
+            d.y = this.radius * Math.cos(theta) + 370;
         }
 
         let diagram = d3.select('#chord-diagram');
 
-        let nodes = diagram
+
+        let nodes = d3.select('#chord-labels')
+            .selectAll('text')
             .data(this.teams)
             .join("text")
-            .attr("dy", "0.31em")
-            // .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
-            // .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-            .text(function(d) { return d; });
+            .attr("x", d => d.x)
+            .attr("y", d => d.y)
+            .text(d=>d.team);
     }
 }
