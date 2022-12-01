0e922726240efa865cb87ba3b5cf04f760b16d33
DAVID PATTERSON <u1207184@umail.utah.edu>
2022-11-30 23:15:17 -0700

    The teams swap to the front when selected.

--- js/chord-diagram.js
+++ js/chord-diagram.js
@@ -1,13 +1,18 @@
 class ChordDiagram {
     constructor(loadedData){
+        this.state =
+        {
+            selectedTeam: null,
+        }
         this.loadedData = loadedData;
         this.teams = [];
         this.teamNames = new Set();
         loadedData.forEach(game => this.teamNames.add(game.Team));
-        this.teamNames.forEach(team => this.teams.push({team: team}) );
+        this.teamNames.forEach(team => this.teams.push({team: team}));
+        console.log(d3.group(loadedData, d => d.Team))
         
-        const imageWidth = 50;
-        const imageHeight = 56;
+        this.imageWidth = 50;
+        this.imageHeight = 56;
 
 
         this.margin = {top: 100, right: 100, bottom: 100, left: 100};
@@ -18,24 +23,55 @@ class ChordDiagram {
         this.radius = this.diameter / 2;
         this.innerRadius = this.radius - 120;
 
+        this.assignPositions()
+
+        d3.select('#chord-images')
+            .selectAll("image")
+            .data(this.teams, d=>d.team)
+            .join('image')
+            .attr('x', d => d.x - this.imageWidth / 2)
+            .attr('y', d => d.y - this.imageHeight / 2)
+            .attr('width', this.imageWidth)
+            .attr('height', this.imageHeight)
+            .attr("xlink:href", d => `logos/${d.team}.png`)
+
+        d3.select('#chord-images').on("click", (d) => {
+            this.state.selectedTeam = d.srcElement.__data__.team;
+            console.log(this.state.selectedTeam)
+            this.assignPositions()
+            this.update()
+        })
+    }
+
+    update(){
+        d3.select('#chord-images')
+            .selectAll("image")
+            .data(this.teams, d=>d.team)
+            .join('image')
+            .transition()
+            .duration(1000)
+            .attr('x', d => d.x - this.imageWidth / 2)
+            .attr('y', d => d.y - this.imageHeight / 2)
+            .attr('width', this.imageWidth)
+            .attr('height', this.imageHeight)
+            .attr("xlink:href", d => `logos/${d.team}.png`)
+    }
+
+    assignPositions(){
         const scale = d3.scaleLinear()
-            .domain([0, this.teams.length])
-            .range([0, 2 * Math.PI]);
-        
+        .domain([0, this.teams.length])
+        .range([0, 2 * Math.PI]);
+        if(this.state.selectedTeam){
+            let team = JSON.parse(JSON.stringify(this.teams.filter(d => d.team === this.state.selectedTeam)[0]))
+            console.log(this.teams.map(d=>d.team).indexOf(this.state.selectedTeam))
+            this.teams = this.teams.filter(d => d.team !== this.state.selectedTeam)
+            this.teams.splice(15, 0, team)
+        }
+        console.log(this.teams)
         for(let [i, d] of this.teams.entries()){
             var theta = scale(i);
             d.x = this.radius * Math.sin(theta)+ 350;
             d.y = this.radius * Math.cos(theta) + 350;
         }
-
-        d3.select('#chord-images')
-            .selectAll("image")
-            .data(this.teams)
-            .join('image')
-            .attr('x', d => d.x - imageWidth / 2)
-            .attr('y', d => d.y - imageHeight / 2)
-            .attr('width', imageWidth)
-            .attr('height', imageHeight)
-            .attr("xlink:href", d => `logos/${d.team}.png`)
     }
 }
