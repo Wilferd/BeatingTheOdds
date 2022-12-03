fef27569ef87d6c04c39ce01d66ed118bd3e9459
DAVID PATTERSON <u1207184@umail.utah.edu>
2022-12-02 19:42:54 -0700

    Added the starting stuff for the season buttons and made the chord diagram into a flex so it doesn't get weird when zooming out

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-async function loadData() {
+var season12_13 = null;
+var season13_14 = null;
+var season14_15 = null;
+var season15_16 = null;
+var season16_17 = null;
+var season17_18 = null;
+var season18_19 = null;
 
-    return await d3.csv('./data/2018-19/vegas.txt');
+
+
+
+async function loadData() {
+    season12_13 = await d3.csv('./data/2012-13/vegas.txt');
+    season13_14 = await d3.csv('./data/2013-14/vegas.txt');
+    season14_15 = await d3.csv('./data/2014-15/vegas.txt');
+    season15_16 = await d3.csv('./data/2015-16/vegas.txt');
+    season16_17 = await d3.csv('./data/2016-17/vegas.txt');
+    season17_18 = await d3.csv('./data/2017-18/vegas.txt');
+    season18_19 = await d3.csv('./data/2018-19/vegas.txt');
+    return season18_19
 }
 
 loadData().then((loadedData) => {
+    console.log(loadedData);
     document.body.style.zoom = "67%";
     const BC = new BarChart(loadedData);
     const lineView = new LineChart(loadedData, BC);
