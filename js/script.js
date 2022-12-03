e92588e748fead1783414cd70ce6a621a09533dc
DAVID PATTERSON <u1207184@umail.utah.edu>
2022-12-02 22:10:06 -0700

    Added the selector for the season!

--- js/script.js
+++ js/script.js
@@ -17,13 +17,44 @@ async function loadData() {
     season16_17 = await d3.csv('./data/2016-17/vegas.txt');
     season17_18 = await d3.csv('./data/2017-18/vegas.txt');
     season18_19 = await d3.csv('./data/2018-19/vegas.txt');
-    return season18_19
+    return season12_13
 }
 
 loadData().then((loadedData) => {
-    console.log(loadedData);
     document.body.style.zoom = "67%";
-    const BC = new BarChart(loadedData);
-    const lineView = new LineChart(loadedData, BC);
-    const chord = new ChordDiagram(loadedData);
+    update(loadedData); 
+    d3.select("#season12-13").on("click", d=> {
+        update(season12_13);
+    });
+    d3.select("#season13-14").on("click", d=> {
+        update(season13_14);
+    }
+    );
+    d3.select("#season14-15").on("click", d=> {
+        update(season14_15);
+    }
+    );
+    d3.select("#season15-16").on("click", d=> {
+        update(season15_16);
+    }
+    );
+    d3.select("#season16-17").on("click", d=> {
+        update(season16_17);
+    }
+    );
+    d3.select("#season17-18").on("click", d=> {
+        update(season17_18);
+    }
+    );
+    d3.select("#season18-19").on("click", d=> {
+        update(season18_19);
+    }
+    );
 });
+
+
+function update(data){
+    const BC = new BarChart(data);
+    const lineView = new LineChart(data, BC);
+    const chord = new ChordDiagram(data);
+}
